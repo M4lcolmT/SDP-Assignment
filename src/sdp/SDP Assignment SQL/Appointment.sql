b5e1cfdc243f780dbf4aead81c95946d0fccff65
WoonHowHin <77482553+WoonHowHin@users.noreply.github.com>
2023-03-15 01:17:15 +0800

    Updated PendingAppointment and Appointment SQL

--- src/sdp/SDP Assignment SQL/Appointment.sql
+++ src/sdp/SDP Assignment SQL/Appointment.sql
@@ -5,15 +5,14 @@ CREATE TABLE Appointment (
 	username VARCHAR (50),
     bloodbank_name VARCHAR(100),
     date DATE,
-    time VARCHAR(100),
-    address VARCHAR(200)
+    time VARCHAR(100)
 );
 
 --To insert values
-INSERT INTO Appointment (username, bloodbank_name, date, time, address) VALUES
-('Lee Wei Han', 'Hospital Kuala Lumpur', '2023-04-20', '9:00 AM', 'Jalan Pahang, 50586 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur'),
-('Lee Wei Han','Hospital Putrajaya', '2023-02-26', '11:00 AM', 'Presint 7, 62250 Putrajaya'),
-('Lee Wei Han','Hospital Putrajaya', '2023-01-15', '11:00 AM', 'Presint 7, 62250 Putrajaya')
+INSERT INTO Appointment (username, bloodbank_name, date, time) VALUES
+('Lee Wei Han', 'Hospital Kuala Lumpur', '2023-04-20', '9:00 AM'),
+('Lee Wei Han','Hospital Putrajaya', '2023-02-26', '11:00 AM'),
+('Lee Wei Han','Hospital Putrajaya', '2023-01-15', '11:00 AM')
 
 
 -- To view all the datas from the table
--- src/sdp/SDP Assignment SQL/PendingAppointment.sql
+++ src/sdp/SDP Assignment SQL/PendingAppointment.sql
@@ -6,18 +6,16 @@ CREATE TABLE Pending_Appointment (
     bloodbank_name VARCHAR(100),
     date DATE,
     time VARCHAR(100),
-    address VARCHAR(200)
 );
 
 --To insert values
-INSERT INTO Pending_Appointment (username, bloodbank_name, date, time, address) VALUES
-('Lee Wei Han', 'Hospital Kuala Lumpur', '2023-04-20', '9:00 AM', 'Jalan Pahang, Kuala Lumpur'),
-('Lee Wei Han','Hospital Putrajaya', '2023-04-26', '4:00 PM', 'Presint 7, 62250 Putrajaya'),
-('Siti','Hospital Putrajaya', '2023-04-15', '1:00 PM', 'Presint 7, 62250 Putrajaya'),
-('Siti','Hospital Putrajaya', '2023-03-15', '2:00 PM', 'Presint 7, 62250 Putrajaya'),
-('Siti','Hospital Putrajaya', '2023-05-15', '3:00 PM', 'Presint 7, 62250 Putrajaya')
+INSERT INTO Pending_Appointment (username, bloodbank_name, date, time) VALUES
+('Lee Wei Han', 'Hospital Kuala Lumpur', '2023-04-20', '9:00 AM'),
+('Lee Wei Han','Hospital Putrajaya', '2023-04-26', '4:00 PM'),
+('Siti','Hospital Putrajaya', '2023-04-15', '1:00 PM'),
+('Siti','Hospital Putrajaya', '2023-03-15', '2:00 PM'),
+('Siti','Hospital Putrajaya', '2023-05-15', '3:00 PM')
 
 
 -- To view all the datas from the table
 SELECT * from Pending_Appointment
-
